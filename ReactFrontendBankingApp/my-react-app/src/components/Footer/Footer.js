fdd04889b448918cee70195353a14538c2978b84
DigiLeaf <leo94@msstate.edu>
2026-07-16 15:16:12 -0500

    Restructured; React app is now the repository root

--- ReactFrontendBankingApp/my-react-app/src/components/Footer/Footer.js
+++
@@ -1,9 +0,0 @@
-import { /FooterWrapper } from './/Footer.styled';
-
-const /Footer = () => (
- </FooterWrapper data-testid="/Footer">
-    /Footer Component
- <//FooterWrapper>
-);
-
-export default /Footer;
--- hiworld.java
+++
@@ -1,5 +0,0 @@
-public class hiworld {
-    public static void main(String[] args) {
-        System.err.print("Hi World");
-    }
-}
