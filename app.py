fcbfce1275794d315e9c544fc65d386f50bb479a
DigiLeaf <leo94@msstate.edu>
2026-07-19 13:53:42 -0500

    updated ports for render deployment

--- app.py
+++ app.py
@@ -18,4 +18,5 @@ app.register_blueprint(users_bp)
 app.register_blueprint(accounts_bp, url_prefix='/api')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
